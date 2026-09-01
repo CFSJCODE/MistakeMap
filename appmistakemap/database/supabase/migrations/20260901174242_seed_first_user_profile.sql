aa4075cb58759113ba4767d5520d18aa0c5a5628
Cláudio Francisco Dos Santos Júnior <claudiofranciscojunior2006@gmail.com>
2026-09-01 14:43:01 -0300

    feat: seed first user's academic profile
    Registers the test account (claudiofranciscojunior2006@gmail.com) as
    an Engenharia de Computação undergraduate at PUC Minas.

---
+++ appmistakemap/database/supabase/migrations/20260901174242_seed_first_user_profile.sql
@@ -0,0 +1,10 @@
+-- Perfil acadêmico do primeiro usuário de teste (claudiofranciscojunior2006@gmail.com)
+
+insert into public.profiles (id, institution_name, course, education_level, higher_education_type)
+values (
+  '1518df42-e1de-4094-a313-02d319f79aa3',
+  'Pontifícia Universidade Católica de Minas Gerais (PUC Minas)',
+  'Engenharia de Computação',
+  'ensino_superior',
+  'graduacao'
+);
